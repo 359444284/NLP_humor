4f78be0ae9cac0caae9590a0f72d166f530edc09
359444284 <47363832+359444284@users.noreply.github.com>
2021-03-01 16:34:05 +0000

    MTL

--- albert.py
+++ albert.py
@@ -2,19 +2,20 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
 from transformers import AlbertTokenizer, AlbertModel, AlbertConfig, AlbertForPreTraining, AdamW, \
-    get_linear_schedule_with_warmup, AlbertForSequenceClassification, AutoModel, AutoTokenizer
+    get_linear_schedule_with_warmup, AlbertForSequenceClassification, AutoModel, AutoTokenizer, \
+    RobertaConfig, RobertaModel, RobertaTokenizer
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, classification_report, roc_curve, auc
 from collections import defaultdict
 import pandas as pd
 import random
 import matplotlib as plt
 
-RANDOM_SEED = 778
+RANDOM_SEED = 750
 BATCH_SIZE = 8
 MAX_LEN = 150
-EPOCHS = 30
+EPOCHS = 20
 torch.cuda.current_device()
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -26,7 +27,6 @@ def set_seed(seed):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
     torch.cuda.manual_seed(seed)
-
 
 class GPReviewDataset(Dataset):
     def __init__(self, reviews, targets, tokenizer, max_len):
@@ -58,11 +58,10 @@ class GPReviewDataset(Dataset):
             'targets': torch.tensor(target, dtype=torch.long)
         }
 
-
 def create_data_loader(df, tokenizer, max_len, batch_size):
     ds = GPReviewDataset(
         reviews=df.text.to_numpy(),
-        targets=df.is_humor.to_numpy(),
+        targets=df.list.to_numpy(),
         tokenizer=tokenizer,
         max_len=max_len
     )
@@ -73,21 +72,35 @@ def create_data_loader(df, tokenizer, max_len, batch_size):
         num_workers=4
     )
 
-
 class MyModel(nn.Module):
-    def __init__(self, freeze_bert=False, n_classes=2):
+    def __init__(self, freeze_bert=False):
         super(MyModel, self).__init__()
-        albert_xxlarge_configuration = AlbertConfig(output_hidden_states=True, output_attentions=True)
-        self.model = AlbertModel.from_pretrained(pretrained_model_name_or_path=MODEL_PATH,
-                                                 config=albert_xxlarge_configuration)
-        # self.model = AutoModel.from_pretrained(pretrained_model_name_or_path=MODEL_PATH)
+        albert_xxlarge_configuration = AlbertConfig(output_hidden_states=True, output_attentions=True, add_pooling_layer=False)
+        self.model = AlbertModel.from_pretrained(pretrained_model_name_or_path=MODEL_PATH, config=albert_xxlarge_configuration)
+        #self.model = RobertaModel.from_pretrained(pretrained_model_name_or_path=MODEL_PATH, output_hidden_states=True, output_attentions=True)
+        #self.model = AutoModel.from_pretrained(pretrained_model_name_or_path=MODEL_PATH)
         if freeze_bert:
             for p in self.model.parameters():
                 p.requires_grad = False
 
-        self.drop = nn.Dropout(p=0.2)
-        self.classifier = nn.Linear(self.model.config.hidden_size, n_classes)
-        self.softmax = nn.Softmax(dim=1)
+        # share layer
+        self.softmax_all_layer = nn.Softmax(-1)
+        self.pooler = nn.Linear(self.model.config.hidden_size, self.model.config.hidden_size)
+        self.pooler_activation = nn.Tanh()
+        self.nn_dense = nn.Linear(self.model.config.hidden_size, 1)
+        self.act = nn.ReLU()
+
+        self.tower_1 = nn.Sequential(
+            nn.Dropout(p=0.8),
+            nn.Linear(self.model.config.hidden_size, 2),
+            nn.Sigmoid()
+        )
+
+        self.tower_2 = nn.Sequential(
+            nn.Dropout(p=0.8),
+            nn.Linear(self.model.config.hidden_size, 2),
+            nn.Sigmoid()
+        )
 
 
     def forward(self, input_ids, attention_mask):
@@ -95,13 +108,24 @@ class MyModel(nn.Module):
             input_ids=input_ids,
             attention_mask=attention_mask
         )
-        pooled_output = outputs[1]
+        layer_logits = []
+        for layer in outputs[2]:
+            out = self.nn_dense(layer)
+            layer_logits.append(self.act(out))
 
-        output = self.drop(pooled_output)
-        output = self.classifier(output)
-        return self.softmax(output)
+        layer_logits = torch.cat(layer_logits, axis=2)
+        layer_dist = self.softmax_all_layer(layer_logits)
+        seq_out = torch.cat([torch.unsqueeze(x, axis=2) for x in outputs[2]], axis=2)
+        pooled_output = torch.matmul(torch.unsqueeze(layer_dist, axis=2), seq_out)
+        pooled_output = torch.squeeze(pooled_output, axis=2)
+
+        pooled_output = self.pooler_activation(self.pooler(pooled_output[:, 0])) if self.pooler is not None else None
+        #pooled_output = outputs[1]
 
 
+        output1 = self.tower_1(pooled_output)
+        output2 = self.tower_2(pooled_output)
+        return output1, output2
 
 def train_epoch(
     model,
@@ -114,44 +138,65 @@ def train_epoch(
 ):
     model = model.train()
     losses = []
-    correct_predictions = 0
+    correct_predictions1 = 0
+    correct_predictions2 = 0
+
     for d in data_loader:
         input_ids = d["input_ids"].to(device)
         attention_mask = d["attention_mask"].to(device)
         targets = d["targets"].to(device)
-        outputs = model(
+        output1, output2 = model(
           input_ids=input_ids,
           attention_mask=attention_mask
         )
-        _, preds = torch.max(outputs, dim=1)
-        loss = loss_fn(outputs, targets)
-        correct_predictions += torch.sum(preds == targets)
+        _, preds1 = torch.max(output1, dim=1)
+        _, preds2 = torch.max(output2, dim=1)
+
+
+        loss1 = loss_fn(output1, targets[:,0])
+        loss2 = loss_fn(output2, targets[:,2])
+        loss = (loss1 + loss2)/2
+
+        correct_predictions1 += torch.sum(preds1 == targets[:,0])
+        correct_predictions2 += torch.sum(preds2 == targets[:,2])
+
+
         losses.append(loss.item())
         loss.backward()
         nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
         scheduler.step()
         optimizer.zero_grad()
-    return correct_predictions.double() / n_examples, np.mean(losses)
+    return correct_predictions1.double() / n_examples, correct_predictions2.double() / n_examples, np.mean(losses)
+
 
 def eval_model(model, data_loader, loss_fn, device, n_examples):
     model = model.eval()
     losses = []
-    correct_predictions = 0
+    correct_predictions1 = 0
+    correct_predictions2 = 0
+
     with torch.no_grad():
         for d in data_loader:
             input_ids = d["input_ids"].to(device)
             attention_mask = d["attention_mask"].to(device)
             targets = d["targets"].to(device)
-            outputs = model(
+            output1, output2 = model(
             input_ids=input_ids,
             attention_mask=attention_mask
             )
-            _, preds = torch.max(outputs, dim=1)
-            loss = loss_fn(outputs, targets)
-            correct_predictions += torch.sum(preds == targets)
+
+            _, preds1 = torch.max(output1, dim=1)
+            _, preds2 = torch.max(output2, dim=1)
+
+            loss1 = loss_fn(output1, targets[:,0])
+            loss2 = loss_fn(output2, targets[:,2])
+            loss = (loss1 + loss2)/2
+
+            correct_predictions1 += torch.sum(preds1 == targets[:,0])
+            correct_predictions2 += torch.sum(preds2 == targets[:,2])
             losses.append(loss.item())
-    return correct_predictions.double() / n_examples, np.mean(losses)
+    return correct_predictions1.double() / n_examples, correct_predictions2.double() / n_examples, np.mean(losses)
 
 def get_predictions(model, data_loader):
     model = model.eval()
@@ -165,15 +210,16 @@ def get_predictions(model, data_loader):
             input_ids = d["input_ids"].to(device)
             attention_mask = d["attention_mask"].to(device)
             targets = d["targets"].to(device)
-            outputs = model(
+            output1, output2 = model(
                 input_ids=input_ids,
                 attention_mask=attention_mask
             )
-            _, preds = torch.max(outputs, dim=1)
+            _, preds1 = torch.max(output1, dim=1)
+            _, preds2 = torch.max(output2, dim=1)
             review_texts.extend(texts)
-            predictions.extend(preds)
-            prediction_probs.extend(outputs)
-            real_values.extend(targets)
+            predictions.extend([preds1, preds2])
+            prediction_probs.extend([output1, output2])
+            real_values.extend([targets[:,0], targets[:,2]])
     predictions = torch.stack(predictions).cpu()
     prediction_probs = torch.stack(prediction_probs).cpu()
     real_values = torch.stack(real_values).cpu()
@@ -187,8 +233,12 @@ if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, output_hidden_states=True, return_dict=True)
 
     df = pd.read_csv("./datas/task1/train/train.csv")
-    df = df[['text', 'is_humor']]
-    class_names = ['is_humor', 'not_humor']
+    df = df.fillna(0.0)
+    df['list'] = df[df.columns[2:]].values.tolist()
+    df = df[['text', 'list']]
+
+    class_names_1 = ['is_humor', 'not_humor']
+    class_names_2 = ['is_CON', 'not_CON']
     df_train, df_test = train_test_split(
         df,
         test_size=0.1,
@@ -205,9 +255,9 @@ if __name__ == '__main__':
     test_data_loader = create_data_loader(df_test, tokenizer, MAX_LEN, BATCH_SIZE)
     data = next(iter(train_data_loader))
 
-    model = MyModel(n_classes=2)
-    # model.load_state_dict(torch.load('./best_model_state.bin'))
-    
+    model = MyModel()
+    #model.load_state_dict(torch.load('./best_model_state.bin'))
+
     if torch.cuda.device_count()>1:
       model=nn.DataParallel(model,device_ids=[0,1,2])
 
@@ -215,7 +265,7 @@ if __name__ == '__main__':
     input_ids = data['input_ids'].to(device)
     attention_mask = data['attention_mask'].to(device)
 
-    optimizer = AdamW(model.parameters(), lr=2e-5, correct_bias=False)
+    optimizer = AdamW(model.parameters(), lr=2e-6, correct_bias=False)
     total_steps = len(train_data_loader) * EPOCHS
 
     scheduler = get_linear_schedule_with_warmup(
@@ -231,7 +281,7 @@ if __name__ == '__main__':
     for epoch in range(EPOCHS):
         print(f'Epoch {epoch + 1}/{EPOCHS}')
         print('-' * 10)
-        train_acc, train_loss = train_epoch(
+        train_acc_1, train_acc_2, train_loss = train_epoch(
             model,
             train_data_loader,
             loss_fn,
@@ -240,40 +290,35 @@ if __name__ == '__main__':
             scheduler,
             len(df_train)
         )
-        print(f'Train loss {train_loss} accuracy {train_acc}')
-        val_acc, val_loss = eval_model(
+        print(f'Train loss {train_loss} accuracy1 {train_acc_1} accuracy2 {train_acc_2}')
+        val_acc_1, val_acc_2, val_loss = eval_model(
             model,
             val_data_loader,
             loss_fn,
             device,
             len(df_val)
         )
-        print(f'Val   loss {val_loss} accuracy {val_acc}')
+        print(f'Val   loss {val_loss} accuracy1 {val_acc_1} accuracy2 {val_acc_2}')
         print()
-        history['train_acc'].append(train_acc)
+        history['train_acc_1'].append(train_acc_1)
+        history['train_acc_2'].append(train_acc_2)
         history['train_loss'].append(train_loss)
-        history['val_acc'].append(val_acc)
+        history['val_acc_1'].append(val_acc_1)
+        history['val_acc_2'].append(val_acc_2)
         history['val_loss'].append(val_loss)
-        if val_acc > best_accuracy:
+        mean_acc = (val_acc_1 + val_acc_2)/2
+        if mean_acc > best_accuracy:
             torch.save(model.state_dict(), 'best_model_state.bin')
-            best_accuracy = val_acc
+            best_accuracy = mean_acc
 
 
 
-    test_acc, _ = eval_model(
-        model,
-        test_data_loader,
-        loss_fn,
-        device,
-        len(df_test)
-    )
-    test_acc.item()
-    print(test_acc.item())
 
     y_review_texts, y_pred, y_pred_probs, y_test = get_predictions(
         model,
         test_data_loader
     )
 
-    print(classification_report(y_test, y_pred, target_names=class_names))
+    print(classification_report(y_test[:,0], y_pred[:,1], target_names=class_names_1))
+    print(classification_report(y_test[:,1], y_pred[:,1], target_names=class_names_2))
 
