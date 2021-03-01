0b04925744119dab3d608362d36d518ad09510b8
359444284 <47363832+359444284@users.noreply.github.com>
2021-03-01 20:50:04 +0000

    4 task

--- albert.py
+++ albert.py
@@ -6,7 +6,7 @@ from transformers import AlbertTokenizer, AlbertModel, AlbertConfig, AlbertForPr
     RobertaConfig, RobertaModel, RobertaTokenizer
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, classification_report, roc_curve, auc
+from sklearn.metrics import confusion_matrix, classification_report, roc_curve, auc, mean_squared_error
 from collections import defaultdict
 import pandas as pd
 import random
@@ -90,16 +90,30 @@ class MyModel(nn.Module):
         self.nn_dense = nn.Linear(self.model.config.hidden_size, 1)
         self.act = nn.ReLU()
 
+        # is_humour
         self.tower_1 = nn.Sequential(
             nn.Dropout(p=0.8),
             nn.Linear(self.model.config.hidden_size, 2),
             nn.Sigmoid()
         )
-
+        
+        # humor_rating
         self.tower_2 = nn.Sequential(
+            nn.Dropout(p=0.8),
+            nn.Linear(self.model.config.hidden_size, 1).clamp(0, 5)
+        )
+        
+        # humor_controversy
+        self.tower_3 = nn.Sequential(
             nn.Dropout(p=0.8),
             nn.Linear(self.model.config.hidden_size, 2),
             nn.Sigmoid()
+        )
+        
+        # offense_rating
+        self.tower_4 = nn.Sequential(
+            nn.Dropout(p=0.8),
+            nn.Linear(self.model.config.hidden_size, 1).clamp(0, 5)
         )
 
 
@@ -125,12 +139,15 @@ class MyModel(nn.Module):
 
         output1 = self.tower_1(pooled_output)
         output2 = self.tower_2(pooled_output)
-        return output1, output2
+        output3 = self.tower_3(pooled_output)
+        output4 = self.tower_4(pooled_output)
+        return output1, output2, output3, output4
 
 def train_epoch(
     model,
     data_loader,
-    loss_fn,
+    loss_fn_CE,
+    loss_fn_MSE,
     optimizer,
     device,
     scheduler,
@@ -145,20 +162,31 @@ def train_epoch(
         input_ids = d["input_ids"].to(device)
         attention_mask = d["attention_mask"].to(device)
         targets = d["targets"].to(device)
-        output1, output2 = model(
+        output1, output2, output3, output4 = model(
           input_ids=input_ids,
           attention_mask=attention_mask
         )
         _, preds1 = torch.max(output1, dim=1)
-        _, preds2 = torch.max(output2, dim=1)
+        mes1 = mean_squared_error(targets[:,1],output2)
+        _, preds3 = torch.max(output3, dim=1)
+        mes2 = mean_squared_error(targets[:,3],output2)
+        
+        
+        loss1 = loss_fn_CE(output1, targets[:,0])
+        loss4 = loss_fn_MSE(output4, targets[:,3])
+        loss = loss1 + loss4
+        if torch.sum(preds1 == 1) != 0:
+            loss2 = loss_fn_MSE(output2[preds1 == 1], targets[:,1][preds1 == 1])
+            loss3 = loss_fn_CE(output3[preds1 == 1], targets[:,2][preds1 == 1])
+            loss = loss2 + loss3
+            loss = loss/4
+        else:
+            loss = loss/2
 
-
-        loss1 = loss_fn(output1, targets[:,0])
-        loss2 = loss_fn(output2, targets[:,2])
-        loss = (loss1 + loss2)/2
-
-        correct_predictions1 += torch.sum(preds1 == targets[:,0])
+        correct_predictions1 += c(preds1 == targets[:,0])
+        acc1 = correct_predictions1.double() / n_examples
         correct_predictions2 += torch.sum(preds2 == targets[:,2])
+        acc2 = correct_predictions2.double() / n_examples
 
 
         losses.append(loss.item())
@@ -167,7 +195,7 @@ def train_epoch(
         optimizer.step()
         scheduler.step()
         optimizer.zero_grad()
-    return correct_predictions1.double() / n_examples, correct_predictions2.double() / n_examples, np.mean(losses)
+    return acc1, mes1, acc2, mes2, np.mean(losses)
 
 
 def eval_model(model, data_loader, loss_fn, device, n_examples):
@@ -181,22 +209,34 @@ def eval_model(model, data_loader, loss_fn, device, n_examples):
             input_ids = d["input_ids"].to(device)
             attention_mask = d["attention_mask"].to(device)
             targets = d["targets"].to(device)
-            output1, output2 = model(
+            output1, output2, output3, output4 = model(
             input_ids=input_ids,
             attention_mask=attention_mask
             )
-
+         
             _, preds1 = torch.max(output1, dim=1)
-            _, preds2 = torch.max(output2, dim=1)
+            mes1 = mean_squared_error(targets[:,1],output2)
+            _, preds3 = torch.max(output3, dim=1)
+            mes2 = mean_squared_error(targets[:,3],output2)
 
-            loss1 = loss_fn(output1, targets[:,0])
-            loss2 = loss_fn(output2, targets[:,2])
-            loss = (loss1 + loss2)/2
+            loss1 = loss_fn_CE(output1, targets[:,0])
+            loss4 = loss_fn_MSE(output4, targets[:,3])
+            loss = loss1 + loss4
+            if torch.sum(preds1 == 1) != 0:
+                loss2 = loss_fn_MSE(output2[preds1 == 1], targets[:,1][preds1 == 1])
+                loss3 = loss_fn_CE(output3[preds1 == 1], targets[:,2][preds1 == 1])
+                loss = loss2 + loss3
+                loss = loss/4
+            else:
+                loss = loss/2
 
-            correct_predictions1 += torch.sum(preds1 == targets[:,0])
+            correct_predictions1 += c(preds1 == targets[:,0])
+            acc1 = correct_predictions1.double() / n_examples
             correct_predictions2 += torch.sum(preds2 == targets[:,2])
+            acc2 = correct_predictions2.double() / n_examples
+
             losses.append(loss.item())
-    return correct_predictions1.double() / n_examples, correct_predictions2.double() / n_examples, np.mean(losses)
+    return acc1, mes1, acc2, mes2, np.mean(losses)
 
 def get_predictions(model, data_loader):
     model = model.eval()
@@ -210,16 +250,20 @@ def get_predictions(model, data_loader):
             input_ids = d["input_ids"].to(device)
             attention_mask = d["attention_mask"].to(device)
             targets = d["targets"].to(device)
-            output1, output2 = model(
+            output1, output2, output3, output4 = model(
                 input_ids=input_ids,
                 attention_mask=attention_mask
             )
+            
             _, preds1 = torch.max(output1, dim=1)
-            _, preds2 = torch.max(output2, dim=1)
+            mes1 = mean_squared_error(targets[:,1],output2)
+            _, preds3 = torch.max(output3, dim=1)
+            mes2 = mean_squared_error(targets[:,3],output2)
+            
             review_texts.extend(texts)
-            predictions.extend([preds1, preds2])
-            prediction_probs.extend([output1, output2])
-            real_values.extend([targets[:,0], targets[:,2]])
+            predictions.extend([preds1, mes1, preds2, mes2])
+            prediction_probs.extend([output1, output2, output3, output4])
+            real_values.extend([targets[:,0], targets[:,1], targets[:,2], targets[:,3]])
     predictions = torch.stack(predictions).cpu()
     prediction_probs = torch.stack(prediction_probs).cpu()
     real_values = torch.stack(real_values).cpu()
@@ -281,7 +325,7 @@ if __name__ == '__main__':
     for epoch in range(EPOCHS):
         print(f'Epoch {epoch + 1}/{EPOCHS}')
         print('-' * 10)
-        train_acc_1, train_acc_2, train_loss = train_epoch(
+        train_acc_1, train_mse_1, train_acc_2, train_mse_2, train_loss = train_epoch(
             model,
             train_data_loader,
             loss_fn,
@@ -291,7 +335,7 @@ if __name__ == '__main__':
             len(df_train)
         )
         print(f'Train loss {train_loss} accuracy1 {train_acc_1} accuracy2 {train_acc_2}')
-        val_acc_1, val_acc_2, val_loss = eval_model(
+        val_acc_1, val_mse_1, val_acc_2, val_mse_1, val_loss = eval_model(
             model,
             val_data_loader,
             loss_fn,
@@ -319,6 +363,8 @@ if __name__ == '__main__':
         test_data_loader
     )
 
-    print(classification_report(y_test[:,0], y_pred[:,1], target_names=class_names_1))
-    print(classification_report(y_test[:,1], y_pred[:,1], target_names=class_names_2))
+    print(classification_report(y_test[:,0], y_pred[:,0], target_names=class_names_1))
+    print(mean_squared_error(y_test[:,1], y_pred[:,1]))
+    print(classification_report(y_test[:,2], y_pred[:,2], target_names=class_names_2))
+    print(mean_squared_error(y_test[:,3], y_pred[:,3]))
 
