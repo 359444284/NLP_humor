61fe44dcd946b78c6b47b752c342e6901c69c0ee
359444284 <47363832+359444284@users.noreply.github.com>
2021-03-02 00:03:48 +0000

    Update albert.py

--- albert.py
+++ albert.py
@@ -179,7 +179,7 @@ def train_epoch(
                              output2[preds1 == 1],
                              output3[preds1 == 1],
                              output4,
-                             [targets[:,1], targets[:,2][preds1 == 1], targets[:,3][preds1 == 1], targets[:,4]]
+                             [targets[:,0], targets[:,1][preds1 == 1], targets[:,2][preds1 == 1], targets[:,3]]
                          )
 
         correct_predictions1 += torch.sum(preds1 == targets[:,0])
@@ -224,7 +224,7 @@ def eval_model(model, mtl, data_loader, loss_fn_CE, loss_fn_MSE, device, n_examp
                                  output2[preds1 == 1],
                                  output3[preds1 == 1],
                                  output4,
-                                 [targets[:,1], targets[:,2][preds1 == 1], targets[:,3][preds1 == 1], targets[:,4]]
+                                 [targets[:,0], targets[:,1][preds1 == 1], targets[:,2][preds1 == 1], targets[:,3]]
                              )
 
             correct_predictions1 += torch.sum(preds1 == targets[:,0])
