de94b7e78db78b6fc8b387ad2a99b78d60a824ef
359444284 <47363832+359444284@users.noreply.github.com>
2021-03-03 15:14:04 +0000

    Update albert_prid.py

--- albert_prid.py
+++ albert_prid.py
@@ -131,16 +131,30 @@ class MyModel(nn.Module):
         self.nn_dense = nn.Linear(self.model.config.hidden_size, 1)
         self.act = nn.ReLU()
 
+        # is_humour
         self.tower_1 = nn.Sequential(
             nn.Dropout(p=0.8),
             nn.Linear(self.model.config.hidden_size, 2),
-            nn.Sigmoid()
+            nn.Softmax(dim=1)
         )
-
+        
+        # humor_rating
         self.tower_2 = nn.Sequential(
             nn.Dropout(p=0.8),
+            nn.Linear(self.model.config.hidden_size, 1)
+        )
+        
+        # humor_controversy
+        self.tower_3 = nn.Sequential(
+            nn.Dropout(p=0.8),
             nn.Linear(self.model.config.hidden_size, 2),
-            nn.Sigmoid()
+            nn.Softmax(dim=1)
+        )
+        
+        # offense_rating
+        self.tower_4 = nn.Sequential(
+            nn.Dropout(p=0.8),
+            nn.Linear(self.model.config.hidden_size, 1)
         )
 
 
@@ -165,13 +179,18 @@ class MyModel(nn.Module):
 
 
         output1 = self.tower_1(pooled_output)
-        output2 = self.tower_2(pooled_output)
-        return output1, output2
+        output2 = self.tower_2(pooled_output).clamp(0, 5)
+        output3 = self.tower_3(pooled_output)
+        output4 = self.tower_4(pooled_output).clamp(0, 5)
+        return output1, output2, output3, output4
+
 
 def train_epoch(
     model,
+    mtl,
     data_loader,
-    loss_fn,
+    loss_fn_CE,
+    loss_fn_MSE,
     optimizer,
     device,
     scheduler,
@@ -186,20 +205,28 @@ def train_epoch(
         input_ids = d["input_ids"].to(device)
         attention_mask = d["attention_mask"].to(device)
         targets = d["targets"].to(device)
-        output1, output2 = model(
+        output1, output2, output3, output4 = model(
           input_ids=input_ids,
           attention_mask=attention_mask
         )
+        output2 = output2[:,0]
+        output4 = output4[:,0]
         _, preds1 = torch.max(output1, dim=1)
-        _, preds2 = torch.max(output2, dim=1)
-
-
-        loss1 = loss_fn(output1, targets[:,0])
-        loss2 = loss_fn(output2, targets[:,2])
-        loss = (loss1 + loss2)/2
+        mes1 = (output2 - targets[:,1]).norm(2).pow(2)
+        _, preds3 = torch.max(output3, dim=1)
+        mes2 = (output4 - targets[:,3]).norm(2).pow(2)
+        
+        loss, log_vars = mtl(output1,
+                             output2[preds1 == 1],
+                             output3[preds1 == 1],
+                             output4,
+                             [targets[:,0].type(torch.cuda.LongTensor), targets[:,1][preds1 == 1], targets[:,2][preds1 == 1].type(torch.cuda.LongTensor), targets[:,3]]
+                         )
 
         correct_predictions1 += torch.sum(preds1 == targets[:,0])
-        correct_predictions2 += torch.sum(preds2 == targets[:,2])
+        acc1 = correct_predictions1.double() / n_examples
+        correct_predictions2 += torch.sum(preds3 == targets[:,2])
+        acc2 = correct_predictions2.double() / n_examples
 
 
         losses.append(loss.item())
@@ -208,9 +235,9 @@ def train_epoch(
         optimizer.step()
         scheduler.step()
         optimizer.zero_grad()
-    return correct_predictions1.double() / n_examples, correct_predictions2.double() / n_examples, np.mean(losses)
+    return acc1, mes1, acc2, mes2, np.mean(losses)
 
-def eval_model(model, data_loader, loss_fn, device, n_examples):
+def eval_model(model, mtl, data_loader, loss_fn_CE, loss_fn_MSE, device, n_examples):
     model = model.eval()
     losses = []
     correct_predictions1 = 0
@@ -221,22 +248,32 @@ def eval_model(model, data_loader, loss_fn, device, n_examples):
             input_ids = d["input_ids"].to(device)
             attention_mask = d["attention_mask"].to(device)
             targets = d["targets"].to(device)
-            output1, output2 = model(
+            output1, output2, output3, output4 = model(
             input_ids=input_ids,
             attention_mask=attention_mask
             )
-
+            output2 = output2[:,0]
+            output4 = output4[:,0]
+         
             _, preds1 = torch.max(output1, dim=1)
-            _, preds2 = torch.max(output2, dim=1)
+            mes1 = (output2 - targets[:,1]).norm(2).pow(2)
+            _, preds3 = torch.max(output3, dim=1)
+            mes2 = (output4 - targets[:,3]).norm(2).pow(2)
 
-            loss1 = loss_fn(output1, targets[:,0])
-            loss2 = loss_fn(output2, targets[:,2])
-            loss = (loss1 + loss2)/2
+            loss, log_vars = mtl(output1,
+                             output2[preds1 == 1],
+                             output3[preds1 == 1],
+                             output4,
+                             [targets[:,0].type(torch.cuda.LongTensor), targets[:,1][preds1 == 1], targets[:,2][preds1 == 1].type(torch.cuda.LongTensor), targets[:,3]]
+                         )
 
             correct_predictions1 += torch.sum(preds1 == targets[:,0])
-            correct_predictions2 += torch.sum(preds2 == targets[:,2])
+            acc1 = correct_predictions1.double() / n_examples
+            correct_predictions2 += torch.sum(preds3 == targets[:,2])
+            acc2 = correct_predictions2.double() / n_examples
+
             losses.append(loss.item())
-    return correct_predictions1.double() / n_examples, correct_predictions2.double() / n_examples, np.mean(losses)
+    return acc1, mes1, acc2, mes2, np.mean(losses)
 
 def get_predictions(model, data_loader):
     model = model.eval()
@@ -248,15 +285,21 @@ def get_predictions(model, data_loader):
             texts = d["review_text"]
             input_ids = d["input_ids"].to(device)
             attention_mask = d["attention_mask"].to(device)
-            output1, output2 = model(
+            output1, output2, output3, output4 = model(
                 input_ids=input_ids,
                 attention_mask=attention_mask
             )
+            output2 = output2[:,0]
+            output4 = output4[:,0]
+            
             _, preds1 = torch.max(output1, dim=1)
-            _, preds2 = torch.max(output2, dim=1)
+            mes1 = (output2 - targets[:,1]).norm(2).pow(2)
+            _, preds3 = torch.max(output3, dim=1)
+            mes2 = (output4 - targets[:,3]).norm(2).pow(2)
+            
             review_texts.extend(texts)
-            predictions.extend(preds1)
-            prediction_probs.extend(output1)
+            predictions.extend([preds1, mes1, preds3, mes2])
+            prediction_probs.extend([output1, output2, output3, output4])
     predictions = torch.stack(predictions).cpu()
     prediction_probs = torch.stack(prediction_probs).cpu()
     return review_texts, predictions, prediction_probs
@@ -273,20 +316,54 @@ def get_predictions1(model, data_loader):
             input_ids = d["input_ids"].to(device)
             attention_mask = d["attention_mask"].to(device)
             targets = d["targets"].to(device)
-            output1, output2 = model(
+            output1, output2, output3, output4 = model(
                 input_ids=input_ids,
                 attention_mask=attention_mask
             )
+            output2 = output2[:,0]
+            output4 = output4[:,0]
+            
             _, preds1 = torch.max(output1, dim=1)
-            _, preds2 = torch.max(output2, dim=1)
+            mes1 = (output2 - targets[:,1]).norm(2).pow(2)
+            _, preds3 = torch.max(output3, dim=1)
+            mes2 = (output4 - targets[:,3]).norm(2).pow(2)
+            
             review_texts.extend(texts)
-            predictions.extend([preds1, preds2])
-            prediction_probs.extend([output1, output2])
-            real_values.extend([targets[:,0], targets[:,2]])
+            predictions.extend([preds1, mes1, preds3, mes2])
+            prediction_probs.extend([output1, output2, output3, output4])
+            real_values.extend([targets[:,0], targets[:,1], targets[:,2], targets[:,3]])
     predictions = torch.stack(predictions).cpu()
     prediction_probs = torch.stack(prediction_probs).cpu()
     real_values = torch.stack(real_values).cpu()
     return review_texts, predictions, prediction_probs, real_values
+
+class MultiTaskLossWrapper(nn.Module):
+    def __init__(self, task_num, loss_function_CE):
+        super(MultiTaskLossWrapper, self).__init__()
+        self.task_num = task_num
+        self.log_vars = nn.Parameter(torch.zeros((task_num)))
+        self.loss_function_CE = loss_function_CE
+        
+    def forward(self, output1, output2, output3, output4, targets):
+        
+
+        precision1 = torch.exp(-2 * self.log_vars[0])
+        loss = torch.sum(precision1 * self.loss_function_CE(output1, targets[0]) + self.log_vars[0], -1)
+        
+        if output2.numel():
+            precision2 = torch.exp(-2 * self.log_vars[1])
+            loss += torch.sum(precision2/2 * (targets[1] - output2) ** 2. + self.log_vars[1], -1)
+        
+        if output2.numel():
+            precision3 = torch.exp(-self.log_vars[2])
+            loss += torch.sum(precision3 * self.loss_function_CE(output3, targets[2]) + self.log_vars[2], -1)
+        
+        precision4 = torch.exp(-2 * self.log_vars[3])
+        loss += torch.sum(precision4/2 * (targets[3] - output4) ** 2. + self.log_vars[3], -1)
+
+        loss = torch.mean(loss)
+
+        return loss, self.log_vars.data.tolist()
 
 if __name__ == '__main__':
 
@@ -295,11 +372,14 @@ if __name__ == '__main__':
     MODEL_PATH = 'albert-xxlarge-v2'
     tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, output_hidden_states=True, return_dict=True)
 
-    df = pd.read_csv("./public_test.csv")
-    df = df[['text']]
+    df = pd.read_csv("./datas/task1/train/train.csv")
+    df = df.fillna(0.0)
+    df['list'] = df[df.columns[2:]].values.tolist()
+    df = df[['text', 'list']]
+     
     class_names_1 = ['is_humor', 'not_humor']
     class_names_2 = ['is_CON', 'not_CON']
-
+    
     test_data_loader = create_data_loader(df, tokenizer, MAX_LEN, BATCH_SIZE)
 
     model = MyModel()
@@ -313,46 +393,56 @@ if __name__ == '__main__':
 
     optimizer = AdamW(model.parameters(), lr=2e-6, correct_bias=False)
 
-    loss_fn = nn.CrossEntropyLoss().to(device)
-
+    loss_fn_CE = nn.CrossEntropyLoss().to(device)
+    loss_fn_MSE = nn.MSELoss().to(device)
+    mtl = MultiTaskLossWrapper(4,loss_fn_CE).to(device)
+    
     y_review_texts, y_pred, y_pred_probs = get_predictions(
         model,
         test_data_loader
     )
     print(np.shape(y_pred))
     result = pd.read_csv("./public_test.csv", header=0)
-    label = pd.DataFrame({'is_humor':y_pred})
-    label = label[['is_humor']]
-    result = pd.concat([result,label],axis=1)
+    label_1 = pd.DataFrame({'is_humor':y_pred[0]})
+    label_1 = label_1[['is_humor']]
+    label_2 = pd.DataFrame({'humor_rating':y_pred[1]})
+    label_2 = label_2[['humor_rating']]
+    label_3 = pd.DataFrame({'humor_controversy':y_pred[2]})
+    label_3 = label_3[['humor_controversy']]
+    label_4 = pd.DataFrame({'offense_rating':y_pred[3]})
+    label_4 = label_4[['offense_rating']]
+    result = pd.concat([result,label_1,label_2,label_3,label_4],axis=1)
     print(result)
     result.to_csv("task1a.csv")
 
-#    df1 = pd.read_csv("./datas/task1/train/train.csv")
-#    df1['list'] = df1[df1.columns[2:]].values.tolist()
-#    df1 = df1[['text', 'list']]
-#    df_train1, df_test1 = train_test_split(
-#        df1,
-#        test_size=0.1,
-#        random_state=RANDOM_SEED
-#    )
-#    df_val1, df_test1 = train_test_split(
-#        df_test1,
-#        test_size=0.5,
-#        random_state=RANDOM_SEED
-#    )
-#
-#    train_data_loader1 = create_data_loader1(df_train1, tokenizer, MAX_LEN, BATCH_SIZE)
-#    val_data_loader1 = create_data_loader1(df_val1, tokenizer, MAX_LEN, BATCH_SIZE)
-#    test_data_loader1 = create_data_loader1(df_test1, tokenizer, MAX_LEN, BATCH_SIZE)
-#    data1 = next(iter(train_data_loader1))
-#
-#
-#    y_review_texts, y_pred, y_pred_probs, y_test = get_predictions1(
-#        model,
-#        test_data_loader1
-#    )
-#
-#    print(classification_report(y_test[:,0], y_pred[:,0], target_names=class_names_1))
-#    print(classification_report(y_test[:,1], y_pred[:,1], target_names=class_names_2))
+   df1 = pd.read_csv("./datas/task1/train/train.csv")
+   df1['list'] = df1[df1.columns[2:]].values.tolist()
+   df1 = df1[['text', 'list']]
+   df_train1, df_test1 = train_test_split(
+       df1,
+       test_size=0.1,
+       random_state=RANDOM_SEED
+   )
+   df_val1, df_test1 = train_test_split(
+       df_test1,
+       test_size=0.5,
+       random_state=RANDOM_SEED
+   )
+
+   train_data_loader1 = create_data_loader1(df_train1, tokenizer, MAX_LEN, BATCH_SIZE)
+   val_data_loader1 = create_data_loader1(df_val1, tokenizer, MAX_LEN, BATCH_SIZE)
+   test_data_loader1 = create_data_loader1(df_test1, tokenizer, MAX_LEN, BATCH_SIZE)
+   data1 = next(iter(train_data_loader1))
+
+
+   y_review_texts, y_pred, y_pred_probs, y_test = get_predictions1(
+       model,
+       test_data_loader1
+   )
+
+   print(classification_report(y_test[:,0], y_pred[:,0], target_names=class_names_1))
+    print((y_test[:,1] - y_pred[:,1]).norm(2).pow(2))
+    print(classification_report(y_test[:,2], y_pred[:,2], target_names=class_names_2))
+    print((y_test[:,3] - y_pred[:,3]).norm(2).pow(2))
 
 
