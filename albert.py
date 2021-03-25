3d14a1877e5ba33da1cc4f5a35810453aad6d047
359444284 <47363832+359444284@users.noreply.github.com>
2021-03-25 00:15:26 +0000

    Update albert.py

--- albert.py
+++ albert.py
@@ -209,7 +209,7 @@ def train_epoch(
 #         loss = loss_fn_CE(output1, targets[:,2].type(torch.cuda.LongTensor))
         loss1 = loss_fn_CE(output1, targets[:,0].type(torch.cuda.LongTensor))
         loss4 = loss_fn_MSE(output4, targets[:,3])
-        loss += 0.05*loss1 + 0.045*loss4
+        loss += 0.05*loss1 + 0.45*loss4
         if output2[preds1 == 1].numel():
             
             loss2 = loss_fn_MSE(output2[preds1 == 1], targets[:,1][preds1 == 1])
