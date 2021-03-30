a7086fff39bdb592929f7c660a61712d290f9945
359444284 <47363832+359444284@users.noreply.github.com>
2021-03-30 22:39:19 +0100

    Update albert.py

--- albert.py
+++ albert.py
@@ -209,13 +209,13 @@ def train_epoch(
 #         loss = loss_fn_CE(output1, targets[:,2].type(torch.cuda.LongTensor))
         loss1 = loss_fn_CE(output1, targets[:,0].type(torch.cuda.LongTensor))
         loss4 = loss_fn_MSE(output4, targets[:,3])
-        loss += 0.95*loss1 + 0.05*loss4
+        loss += 0.85*loss1 + 0.075*loss4
         if output2[preds1 == 1].numel():
             
             loss2 = loss_fn_MSE(output2[preds1 == 1], targets[:,1][preds1 == 1])
             
             loss3 = loss_fn_CE(output3[preds1 == 1], targets[:,2][preds1 == 1].type(torch.cuda.LongTensor))
-            loss += 0.00*loss2 + 0.00*loss3
+            loss += 0.075*loss2 + 0.00*loss3
             loss = loss
         else:
             loss = loss
@@ -286,13 +286,13 @@ def eval_model(model, mtl, data_loader, loss_fn_CE, loss_fn_MSE, device, n_examp
 #             loss = loss_fn_CE(output1, targets[:,2].type(torch.cuda.LongTensor))
             loss1 = loss_fn_CE(output1, targets[:,0].type(torch.cuda.LongTensor))
             loss4 = loss_fn_MSE(output4, targets[:,3])
-            loss += 0.95*loss1 + 0.05*loss4
+            loss += 0.85*loss1 + 0.075*loss4
             if output2[preds1 == 1].numel():
 
                 loss2 = loss_fn_MSE(output2[preds1 == 1], targets[:,1][preds1 == 1])
 
                 loss3 = loss_fn_CE(output3[preds1 == 1], targets[:,2][preds1 == 1].type(torch.cuda.LongTensor))
-                loss += 0.00*loss2 + 0.00*loss3
+                loss += 0.075*loss2 + 0.00*loss3
                 loss = loss
             else:
                 loss = loss
