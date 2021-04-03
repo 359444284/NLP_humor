b2dd62a72b18ac6180b28e8b1d4521cfd9902914
359444284 <47363832+359444284@users.noreply.github.com>
2021-04-03 05:57:18 +0100

    Update roberta.py

--- roberta.py
+++ roberta.py
@@ -239,6 +239,8 @@ def train_epoch(
         
         loss, log_vars = mtl(output1,
                              output2[preds1 == 1],
+                             output3[preds1 == 1],
+                             output4,
                              [targets[:,0].type(torch.cuda.LongTensor), targets[:,1][preds1 == 1], targets[:,2][preds1 == 1].type(torch.cuda.LongTensor), targets[:,3]]
                              )
 
@@ -312,6 +314,8 @@ def eval_model(model, mtl, data_loader, loss_fn_CE, loss_fn_MSE, device, n_examp
         
             loss, log_vars = mtl(output1,
                                  output2[preds1 == 1],
+                                 output3[preds1 == 1],
+                                 output4,
                                  [targets[:,0].type(torch.cuda.LongTensor), targets[:,1][preds1 == 1], targets[:,2][preds1 == 1].type(torch.cuda.LongTensor), targets[:,3]]
                                  )
             correct_predictions1 += torch.sum(preds1 == targets[:,0])
